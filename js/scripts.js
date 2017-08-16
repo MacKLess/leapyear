85e3abad7d02e9d109f50d3b466ed0cd266027e0
Mitchell Long and LDMacKrell <mitchell@>
2017-08-16 10:19:01 -0700

    add functionality for all contributing components of leap year.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 //business logic
 var leapYear = function(year) {
-  if (year % 4 === 0){
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0))  {
     return true;
   }
   else {
     return false;
   }
-
 };
-
 
 //userinterface logic
 $(document).ready(function(){
