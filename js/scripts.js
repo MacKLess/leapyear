a5b47444e24b402e15ae90cf9f446734d3aa8dd6
Mitchell Long and LDMacKrell <mitchell@>
2017-08-16 16:47:04 -0700

    add for loop functions for vowels and non letter input.

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
   $("form#leap-year").submit(function(event){
     event.preventDefault();
     var year = parseInt($("#year").val());
-    var result = leapYear(year)
+    var result = leapYear(year);
     console.log(result);
 
     $("#outPutYear").text(year);
